aaf585dec7d2e5fd1292c167a67e553409bedf0b
Md. Saiful Islam <saiful9122113@gmail.com>
2022-11-30 00:35:26 +0600

    Api added for email query

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -13,13 +14,42 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbe4zil.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    // console.log('token inside VerifyJWT',req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run(){
     try{
         const productCollection = client.db('resaleProduct').collection('product');
         const userRoleCollection = client.db('resaleProduct').collection('userRole');
         const orderCollection = client.db('resaleProduct').collection('order');
+
+        app.get('/jwt', async(req, res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await userRoleCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''})
+        })
 
         // Api for adding Product 
         app.post('/add-product', async(req, res)=>{
@@ -32,9 +62,23 @@ async function run(){
         // Api for post user & seller information 
         app.post('/user-role', async(req, res)=>{
             const role = req.body;
-            const result = await userRoleCollection.insertOne(role);
-            res.send(result);
+            const isEmailExist = await userRoleCollection.findOne({email: role.email})
+            if(!isEmailExist) {
+                await userRoleCollection.insertOne(role);
+                const userInfo = await userRoleCollection.findOne({email: role.email})
+                return res.send(userInfo);
+            }
+
+            return res.send(isEmailExist);
         });
+
+        app.get("/get-user-info/:email", async(req, res) => {
+            const email = req.params.email;
+
+            const userInfo = await userRoleCollection.findOne({email});
+
+            return res.send(userInfo);
+        })
 
         // Api for sellers own product 
         app.get('/products/:email', async (req, res) => {
@@ -60,6 +104,9 @@ async function run(){
         // Api for all seller 
         app.get('/category/:role', async (req, res) => {
             const query = { role: req.params.role };
+            // const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+            // console.log(email, decodedEmail);
             const result = await userRoleCollection.find(query).toArray();
             res.send(result);
         });
@@ -70,34 +117,6 @@ async function run(){
             const result = await orderCollection.find(query).toArray();
             res.send(result);
         });
-
-        //   app.get('/review', async(req, res) =>{
-        //     // console.log(req.query.email);
-        //     let query = {};
-        //     if(req.query.email){
-        //         query={
-        //             email: req.query.email
-        //         }
-        //     }
-        //     const review = await reviewCollection.find(query).toArray();
-        //     res.send(review);
-        // })
-
-        // API for get products 
-        // app.get('/products', async(req, res)=>{
-        //     const query = {_id: ObjectId(id)};
-        //     const cursor = productCollection.find(query);
-        //     console.log(productCollection);
-        //     const products = await cursor.toArray();
-        //     return res.send(products);
-        // })
-
-        // app.get('/services/:id', async(req, res)=>{
-        //     const id = req.params.id;
-        //     const query ={_id: ObjectId(id)};
-        //     const service = await serviceCollection.findOne(query);
-        //     res.send(service);
-        // })
 
         
     }
